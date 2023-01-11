324dc936146b361bf02b84d3d14561e07cd71d00
Pato Programador <spartan117947@hotmail.com>
2023-01-11 18:49:01 -0500

    Creación de los controlles get por id, put y delete

--- src/controllers/projectsController.js
+++ src/controllers/projectsController.js
@@ -1,13 +1,29 @@
-import { Project } from '../models/Project.js'
+import { Project } from '../models/Project.js';
 
 export const getProjects = async (req, res) => {
     try {
         const projects = await Project.findAll();
-        res.json(projects)
+        res.json(projects);
     } catch (error) {
-        return res.status(500).json({message: error.message})
-    }
-}
+        return res.status(500).json({message: error.message});
+    };
+};
+
+export const getProject = async (req,res) => {
+    try {
+        const {id} = req.params;
+        const project = await Project.findOne({
+            where: {
+                id: id
+            }
+        });
+        
+        if(!project) return res.status(404).json({message: `El proyecto de id: ${id} no existe`});
+        res.json(project);
+    } catch (error) {
+        return res.status(500).json({message: error.message});
+    };
+};
 
 export const createProject = async (req, res) => {
     const { name, priority, description } = req.body;
@@ -15,6 +31,37 @@ export const createProject = async (req, res) => {
         const newProject = await Project.create({ name, priority, description });
         res.json(newProject);
     } catch (error) {
-        return res.status(500).json({message: error.message})
-    }
-}
+        return res.status(500).json({message: error.message});
+    };
+};
+
+export const upadateProject = async (req,res) => {
+    try {
+        const {id} = req.params;
+        const {name,priority,description} = req.body;
+    
+        const project = await Project.findByPk(id);
+        project.name = name;
+        project.priority = priority;
+        project.description = description;
+        await project.save();
+    
+        res.json(project);
+    } catch (error) {
+        return res.status(500).json({message: error.message});
+    };
+};
+
+export const deleteProject = async (req,res) => {
+    try {
+        const {id} = req.params;
+        await Project.destroy({
+            where: {
+                id: id
+            }
+        });
+        res.sendStatus(204);
+    } catch (error) {
+        return res.status(500).json({message: error.message});
+    };
+};
--- src/routes/projectsRoutes.js
+++ src/routes/projectsRoutes.js
@@ -1,12 +1,12 @@
 import {Router} from 'express'
-import { getProjects, createProject } from '../controllers/projectsController.js';
+import { getProjects, createProject, upadateProject, deleteProject, getProject } from '../controllers/projectsController.js';
 
-const router = Router()
+const router = Router();
 
 router.get('/projects', getProjects);
-router.post('/projects', createProject)
-router.put('/projects/:id')
-router.delete('/projects/:id')
-router.get('/projects/:id')
+router.post('/projects', createProject);
+router.put('/projects/:id', upadateProject);
+router.delete('/projects/:id', deleteProject);
+router.get('/projects/:id', getProject);
 
 export default router;
