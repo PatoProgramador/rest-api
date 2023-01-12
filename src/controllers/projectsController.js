3edbdf0c99d9f0794566af5e7daf3a4952f36954
Pato Programador <spartan117947@hotmail.com>
2023-01-11 20:30:26 -0500

    Pequeña practica para usar las relaciones entre tablas

--- src/controllers/projectsController.js
+++ src/controllers/projectsController.js
@@ -1,4 +1,5 @@
 import { Project } from '../models/Project.js';
+import { Task } from '../models/Task.js';
 
 export const getProjects = async (req, res) => {
     try {
@@ -64,4 +65,14 @@ export const deleteProject = async (req,res) => {
     } catch (error) {
         return res.status(500).json({message: error.message});
     };
+};
+
+export const getProjectTasks = async (req,res) => {
+    const {id} = req.params;
+    try {
+        const tasks = await Task.findAll({where: {projectId: id}});
+        res.json(tasks);
+    } catch (error) {
+        return res.status(500).json({message: error.message});
+    }
 };
--- src/routes/projectsRoutes.js
+++ src/routes/projectsRoutes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { getProjects, createProject, upadateProject, deleteProject, getProject } from '../controllers/projectsController.js';
+import { getProjects, createProject, upadateProject, deleteProject, getProject, getProjectTasks } from '../controllers/projectsController.js';
 
 const router = Router();
 
@@ -8,5 +8,7 @@ router.post('/projects', createProject);
 router.put('/projects/:id', upadateProject);
 router.delete('/projects/:id', deleteProject);
 router.get('/projects/:id', getProject);
+
+router.get('/projects/:id/tasks', getProjectTasks);
 
 export default router;
