49d9dde61f0ac3f244ea6df919b91ee3b63f08af
Pato Programador <spartan117947@hotmail.com>
2023-01-11 20:04:30 -0500

    Creación de los controllers put, delete y get by id de Tasks

--- src/controllers/tasksControllers.js
+++ src/controllers/tasksControllers.js
@@ -1,19 +1,57 @@
-import {Task} from '../models/Task.js'
+import { Task } from '../models/Task.js'
 
 export const getTasks = async (req, res) => {
     try {
         const tasks = await Task.findAll();
         res.json(tasks);
     } catch (error) {
-        return res.status(500).json({message: error.message})
+        return res.status(500).json({ message: error.message })
     }
 };
 export const createTask = async (req, res) => {
+    const { name, done, projectId } = req.body;
     try {
-        const { name, done, projectId } = req.body;
-        const newTask = await Task.create({name, done, projectId});
+        const newTask = await Task.create({ name, done, projectId });
         res.json(newTask);
     } catch (error) {
-        return res.status(500).json({message: error.message});
+        return res.status(500).json({ message: error.message });
     };
+};
+
+export const getTask = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const task = await Task.findByPk({
+            where: { id }
+        })
+        res.json(task);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+export const updateTask = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const task = await Task.findOne({
+            where: {id}
+        })
+        task.set(req.body);
+        await task.save();
+        return res.json(task);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    };
+};
+
+export const deleteTask = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await Task.destroy({
+            where: { id }
+        });
+        return res.sendStatus(204);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
 };
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getTasks, createTask } from "../controllers/tasksControllers.js";
+import { getTasks, createTask, updateTask, deleteTask, getTask } from "../controllers/tasksControllers.js";
 
 const router = Router();
 
-router.get('/tasks',getTasks)
-router.post('/tasks',createTask)
-router.put('/tasks/:id')
-router.delete('/tasks/:id')
-router.get('/tasks/:id')
+router.get('/tasks',getTasks);
+router.post('/tasks',createTask);
+router.put('/tasks/:id', updateTask);
+router.delete('/tasks/:id', deleteTask);
+router.get('/tasks/:id', getTask);
 
 export default router;
